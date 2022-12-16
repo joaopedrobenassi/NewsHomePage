202db0db59c83645bd7026ec5e3c9fe17261d36d
joaopedrobenassi <joaopedrobenassi@gmail.com>
2022-12-16 16:23:17 -0300

    selecionando elementos

--- arquivos/js/script.js
+++ arquivos/js/script.js
@@ -1,2 +1,5 @@
 // Iniciando a lógica de interatividade do menu interativo
 
+const icone_interativo = document.querySelector('.imagem_interativa')
+const menu_navegacao = document.querySelector('.menu_navegacao')
+console.log(menu_navegacao.style.background.url)
